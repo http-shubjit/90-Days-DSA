231c81d6187642ed4ce109f44ff4392ad4dd85bb
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-13 17:33:53 +0530

    first commit

---
+++ day9.js
@@ -0,0 +1,34 @@
+//** Write a program to prints factorial of any number**//
+// const factorial = (num) => {
+//   let res = 1;
+//   while (num > 0) {
+//     res = res * num;
+//     num--;
+//   }
+//   return res;
+// };
+// console.log(factorial(5));
+
+//**Write a program for check number is prime or not**//
+
+// const checkPrime = (num) => {
+//   if (num % 2 === 0) return false;
+//   else return true;
+// };
+
+// console.log(checkPrime(2));
+
+//**Write a program to check whether number is perfect number or not**//
+
+// const checkPerfect = (num) => {
+//   let res = 0;
+//   let i = 0;
+//   while (i < num) {
+//     if (num % i === 0) res = res + i;
+//     i++;
+//   }
+//   if (res === num) return true;
+//   else return false;
+// };
+
+// console.log(checkPerfect(7));
