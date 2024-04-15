918d3b850415182ca41ea3b220fd48e917d40eda
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-15 08:50:23 +0530

    first commit

--- day11.js
+++ day11.js
@@ -41,3 +41,23 @@
 // };
 
 // console.log(findDuplicate([1, 2, 3, 4, 1, 3]));
+
+//**Find the largest and smallest number in an unsorted array of integers**//
+
+// const findLargeSmall = (arr) => {
+//   let large, small;
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] <= arr[i + 1]) {
+//       large = arr[i + 1];
+//     }
+//   }
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] >= arr[i + 1]) {
+//       small = arr[i + 1];
+//     }
+//   }
+
+//   return { large: large, small: small };
+// };
+
+// console.log(findLargeSmall([3, 2, 4, 1]));
