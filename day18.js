aaf65f0ac3c23ef438fd89b026008fb88279778b
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-24 17:48:29 +0530

    first commit

--- day18.js
+++ day18.js
@@ -1,4 +1,4 @@
-//**Consider a string, sentence, of words separated by spaces where each word is a substring consisting of English alphabetic letters only. Find the first word in the sentence that has a length which is both an even number and greater than or equal to the length of any other word of even length in the sentence. If there are multiple words meeting the criteria, return the one which occurs first in the sentence.
+//**Find The Longest even no of a sentence**//
 // function findFirstLongestEvenWord(sentence) {
 //   const words = sentence.split(" ");
 //   let longestEvenWord = "";
