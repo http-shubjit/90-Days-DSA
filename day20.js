245895d00fa4a86a751f8458ade03cd25e5fec57
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-25 23:06:43 +0530

    first commit

---
+++ day20.js
@@ -0,0 +1,35 @@
+//**Find K most occurring elements in the given Array**/
+
+///Input: arr[] = {3, 1, 4, 4, 5, 2, 6, 1}, K = 2
+//Output: 4 1
+
+const arr = [3, 1, 4, 4, 5, 2, 6, 1];
+
+const findMustOccurrenceNo = (arr) => {
+  if (arr.length === 0) return "Array is empty";
+
+  let frequency = {};
+
+  for (let val of arr) {
+    if (frequency[val]) {
+      frequency[val]++;
+    } else {
+      frequency[val] = 1;
+    }
+  }
+
+  let maxFrequency = 0;
+  let mostOccurringNumber;
+
+  for (let key in frequency) {
+    console.log(key + ":" + frequency[key]);
+    if (frequency[key] >= maxFrequency) {
+      maxFrequency = frequency[key];
+      mostOccurringNumber = key;
+    }
+  }
+
+  return mostOccurringNumber;
+};
+
+console.log(findMustOccurrenceNo(arr));
