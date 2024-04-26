8bf5af5b6fb98f0eb0071147d2d9b98cf5da84aa
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-26 23:08:38 +0530

    first commit

--- day20.js
+++ day20.js
@@ -1,35 +1,33 @@
-//**Find K most occurring elements in the given Array**/
+// //**Find K most occurring elements in the given Array**/
 
-///Input: arr[] = {3, 1, 4, 4, 5, 2, 6, 1}, K = 2
-//Output: 4 1
+// ///Input: arr[] = {3, 1, 4, 4, 5, 2, 6, 1}, K = 2
+// //Output: 4 1
 
-const arr = [3, 1, 4, 4, 5, 2, 6, 1];
+// const arr = [3, 1, 4, 4, 5, 2, 6, 1, "raka"];
 
-const findMustOccurrenceNo = (arr) => {
-  if (arr.length === 0) return "Array is empty";
+// const findMustOccurrenceNo = (arr) => {
+//   if (arr.length === 0) return "Array is empty";
 
-  let frequency = {};
+//   let frequency = {};
 
-  for (let val of arr) {
-    if (frequency[val]) {
-      frequency[val]++;
-    } else {
-      frequency[val] = 1;
-    }
-  }
+//   for (let val of arr) {
+//     if (frequency[val]) {
+//       frequency[val]++;
+//     } else {
+//       frequency[val] = 1;
+//     }
+//   }
+//   let maxFrequency = 0;
+//   let mostOccurringNumber;
+//   for (let key in frequency) {
+//     console.log(frequency["key"]);
+//     if (frequency[key] >= maxFrequency) {
+//       maxFrequency = frequency[key];
+//       mostOccurringNumber = key;
+//     }
+//   }
 
-  let maxFrequency = 0;
-  let mostOccurringNumber;
+//   return mostOccurringNumber;
+// };
 
-  for (let key in frequency) {
-    console.log(key + ":" + frequency[key]);
-    if (frequency[key] >= maxFrequency) {
-      maxFrequency = frequency[key];
-      mostOccurringNumber = key;
-    }
-  }
-
-  return mostOccurringNumber;
-};
-
-console.log(findMustOccurrenceNo(arr));
+// console.log(findMustOccurrenceNo(arr));
---
+++ day21.js
@@ -0,0 +1,22 @@
+//**How Can We Acces a object with no and with string **//
+
+const obj = {
+  1: "John Doe",
+  2: 30,
+  occupation: "Software Engineer",
+};
+
+// for (let key in obj) {
+//   console.log(key);
+//   console.log(obj[key]);
+// }
+
+// console.log(obj["occupation"]);
+// console.log(obj.occupation);
+// console.log(obj[1]);
+// console.log(obj["1"]);
+
+// obj[2] = obj[2] - 10;
+obj["2"] = obj["2"] - 10;
+
+console.log(obj);
