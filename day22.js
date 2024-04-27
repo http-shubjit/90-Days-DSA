40d178ff3e8626c3a54a3e78edb77318afb2848e
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-27 17:29:29 +0530

    first commit

--- day22.js
+++ day22.js
@@ -1,23 +1,23 @@
-//**Array Rotations**//
-let larr = [1, 2, 3, 4, 5, 6];
-let rarr = [1, 2, 3, 4, 5, 6];
+// //**Array Rotations**//
+// let larr = [1, 2, 3, 4, 5, 6]; //output=[ 3, 4, 5, 6, 1, 2 ]
+// let rarr = [1, 2, 3, 4, 5, 6]; //output=[ 5, 6, 1, 2, 3, 4 ]
 
-let d = 2;
+// let d = 2;
 
-const leftRotaion = (arr, d) => {
-  for (let i = 0; i < d; i++) {
-    let l = arr.shift();
-    arr.push(l);
-  }
-  return arr;
-};
+// const leftRotaion = (arr, d) => {
+//   for (let i = 0; i < d; i++) {
+//     let l = arr.shift();
+//     arr.push(l);
+//   }
+//   return arr;
+// };
 
-const rightRotation = (arr, d) => {
-  for (let i = 0; i < d; i++) {
-    let l = arr.pop();
-    arr.unshift(l);
-  }
-  return arr;
-};
-console.log("RightRotation : " + rightRotation(larr, d));
-console.log("LeftRotaion : " + leftRotaion(rarr, d));
+// const rightRotation = (arr, d) => {
+//   for (let i = 0; i < d; i++) {
+//     let l = arr.pop();
+//     arr.unshift(l);
+//   }
+//   return arr;
+// };
+// console.log(leftRotaion(rarr, d));
+// console.log(rightRotation(larr, d));
