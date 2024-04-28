d40028275a9aeffad605f36a25eca7e0b2158aee
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-28 22:07:31 +0530

    first commit

---
+++ day23.js
@@ -0,0 +1,13 @@
+//**Print all Distinct ( Unique ) Elements in given Array**//
+
+// const arr = [12, 10, 9, 45, 2, 10, 10, 45];
+// const printAlldistnict = (arr) => {
+//   let res = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!res.includes(arr[i])) {
+//       res.push(arr[i]);
+//     }
+//   }
+//   return res;
+// };
+// console.log(printAlldistnict(arr));
