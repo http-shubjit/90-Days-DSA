8c5cda6f586e40b4ab9a6ee04efd582fe35dda26
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-04-29 07:26:17 +0530

    first commit

---
+++ day24.js
@@ -0,0 +1,18 @@
+//**Segregate even and odd numbers using Lomuto’s Partition Scheme**//
+
+// const arr = [1, 3, 2, 4, 7, 6, 9, 10];
+// const findOddandEven = (arr) => {
+//   let i = 0;
+//   let j = 0;
+//   while (j < arr.length) {
+//     if (arr[j] % 2 === 0) {
+//       const temp = arr[i];
+//       arr[i] = arr[j];
+//       arr[j] = temp;
+//       i++;
+//     }
+//     j++;
+//   }
+//   return arr;
+// };
+// console.log(findOddandEven(arr));
