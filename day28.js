79218cf04e0ed80e400784a0a11ad3ea3494642c
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-05-03 08:08:56 +0530

    DSA For You

---
+++ day28.js
@@ -0,0 +1,29 @@
+//**Find Subarray with given sum**/
+//Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+// Output: Sum found between indexes 2 and 4
+// const arr = [1, 4, 20, 3, 10, 5];
+// const sum = 5;
+
+// function findSubarray(arr, sum) {
+//   if (!arr) return;
+//   let ressum;
+//   let resarr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     ressum = arr[i];
+//     if (sum === arr[i]) {
+//       resarr.push({ i: i });
+//     }
+//     for (let j = i + 1; j < arr.length; j++) {
+//       ressum = ressum + arr[j];
+//       if (ressum === sum) {
+//         while (i <= j) {
+//           resarr.push(i);
+//           i++;
+//         }
+//       }
+//     }
+//   }
+
+//   return resarr;
+// }
+// console.log(findSubarray(arr, sum));
