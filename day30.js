6f33af5c1f6bd85555cc509473cba20bdab4a4e4
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-05-05 10:05:17 +0530

    DSA For You

--- day30.js
+++ day30.js
@@ -6,44 +6,44 @@
 // Input: [-1, 3, -2, -4, 7, -5];
 // Output: [ 7, -5, 3, -4, -2, -1];
 
-const arr = [-1, 3, -2, -4, 7, -5];
+// const arr = [-1, 3, -2, -4, 7, -5];
 
-const rearrangeNumber = (arr) => {
-  let res = [];
-  let parr = [];
-  let narr = [];
+// const rearrangeNumber = (arr) => {
+//   let res = [];
+//   let parr = [];
+//   let narr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      parr.push(arr[i]);
-    } else {
-      narr.push(arr[i]);
-    }
-  }
-  parr.sort((a, b) => b - a);
-  narr.sort((a, b) => a - b);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       parr.push(arr[i]);
+//     } else {
+//       narr.push(arr[i]);
+//     }
+//   }
+//   parr.sort((a, b) => b - a);
+//   narr.sort((a, b) => a - b);
 
-  for (let j = 0, n = 0, p = 0; j < arr.length; j++) {
-    if (j % 2 !== 0) {
-      if (n < narr.length) {
-        res.push(narr[n]);
-        n++;
-      } else {
-        res.push(parr[p]);
-        p++;
-      }
-    } else {
-      if (p < parr.length) {
-        res.push(parr[p]);
-        p++;
-      } else {
-        res.push(narr[n]);
-        n++;
-      }
-    }
-  }
+//   for (let j = 0, n = 0, p = 0; j < arr.length; j++) {
+//     if (j % 2 !== 0) {
+//       if (n < narr.length) {
+//         res.push(narr[n]);
+//         n++;
+//       } else {
+//         res.push(parr[p]);
+//         p++;
+//       }
+//     } else {
+//       if (p < parr.length) {
+//         res.push(parr[p]);
+//         p++;
+//       } else {
+//         res.push(narr[n]);
+//         n++;
+//       }
+//     }
+//   }
 
-  return res;
-};
+//   return res;
+// };
 
-console.log(rearrangeNumber(arr));
+// console.log(rearrangeNumber(arr));
