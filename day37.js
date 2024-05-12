6abf617116cd3a095687bf4a0fe892dc824972d7
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-05-12 21:31:29 +0530

    DSA For You

--- day37.js
+++ day37.js
@@ -1,3 +1,12 @@
+// Inversion count in Array
+// Input: arr[] = {8, 4, 2, 1}
+// Output: 6
+// Explanation: Given array has six inversions: (8, 4), (4, 2), (8, 2), (8, 1), (4, 1), (2, 1).
+
+// Input: arr[] = {1, 20, 6, 4, 5}
+// Output: 5
+// Explanation: Given array has five inversions: (20, 6), (20, 4), (20, 5), (6, 4), (6, 5).
+
 const arr = [8, 4, 2, 1];
 
 const inversionCount = (arr) => {
