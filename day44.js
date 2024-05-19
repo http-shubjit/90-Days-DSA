5e70d525488022342244f48ad2e4c1e3dfb470fd
Shubhajit <shubjitgithub@gmail.com>
2024-05-19 16:49:44 +0530

    DSA For You

--- day44.js
+++ day44.js
@@ -1,3 +1,24 @@
-console.log("Hello World");
-console.log("Hello World");
+// Majority Element
 
+// const arr=[3, 3, 4, 2, 4, 4, 2, 4, 4]
+// const findMajorelment=()=>{
+ 
+//     let maxcount=0;
+//     let res;
+
+//     for(let i=0;i<arr.length;i++){
+//         let count=0
+//         for(let j=i+1;j<arr.length;j++){
+//     if(arr[i]===arr[j]){
+//     count++
+//     }
+//  }
+//  if(count>maxcount){
+//     maxcount=count
+//     res=arr[i]
+//  }
+//     }
+//     return res
+
+// }
+// console.log(findMajorelment());
