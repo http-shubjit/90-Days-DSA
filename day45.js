50727cd2373f192d2444419c318f074c772f0b47
Shubhajit <shubjitgithub@gmail.com>
2024-05-20 07:14:09 +0530

    DSA For You

---
+++ day45.js
@@ -0,0 +1,17 @@
+// Two Pointers Technique
+
+// const arr = [10, 20, 35, 50, 75, 80];
+// const k = 80;
+
+// const findElement = (arr, x) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = arr.length - 1; j >= 0; j--) {
+//       if (arr[i] + arr[j] > x) continue;
+//       else if(arr[i]+arr[j]===x) return true
+//       else if(arr[i]+arr[j]<x) break
+//     }
+//   }
+// return false
+// };
+
+// console.log(findElement(arr,k));
