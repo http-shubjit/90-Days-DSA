0551aef6aee66b2e8e1f74e3a2fcc5e3ab1ff7de
Shubhajit <shubjitgithub@gmail.com>
2024-05-24 20:20:56 +0530

    DSA For You

---
+++ day49.java
@@ -0,0 +1,30 @@
+// import java.util.Scanner;
+
+// class Main {
+//     int resarr[] = new int[12];
+//     Scanner sc = new Scanner(System.in);
+
+//     Main(int arr[], int k) {
+
+//         for (int i = 0; i < arr.length; i++) {
+//             int count = 0;
+//             for (int j = i; j < arr.length; j++) {
+//                 if (arr[i] == arr[j]) {
+//                     count++;
+//                 }
+//             }
+//             if (count == k) {
+//               System.out.println(arr[i]); 
+//             }
+//         }
+//     }
+
+
+
+//     public static void main(String[] args) {
+//         int arr[] = { 3, 1, 4, 4, 5, 2, 6, 1 };
+//         int k = 2;
+//         Main ob = new Main(arr, k);
+
+//     }
+// }
---
+++ day49.js
@@ -0,0 +1,22 @@
+
+// const arr = [3, 1, 4, 4, 5, 2, 6, 1];
+// const k = 2;
+
+// const findmostOccuranceNo = (arr, k) => {
+//     let resarr = [];
+//     for (let i = 0; i < arr.length; i++){
+//         let count=0;
+//         for (let j = i; j < arr.length; j++){
+            
+//             if (arr[i] === arr[j]) {
+//                 count++
+//             }
+//         }
+//         if (count === k) {
+//             resarr.push(arr[i])
+//         }
+//     }
+    
+//     return resarr
+// }
+// console.log(findmostOccuranceNo(arr,k));
