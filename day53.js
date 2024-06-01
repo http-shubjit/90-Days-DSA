47fffb5c9a0d805025e87018bba31ba19f482dfa
Shubhajit <shubjitgithub@gmail.com>
2024-06-01 17:31:09 +0530

    DSA For You

--- day53.js
+++ day53.js
@@ -2,8 +2,8 @@
 // Input: arr[] = {5, 7, 5, 2, 10, 12 ,11, 17, 14, 1, 44}
 // query[] = {{2, 5}, {5, 10}, {0, 10}}
 // Output: 60,15708, 78540
-const arr = [5, 7, 5, 2, 10, 12, 11, 17, 14, 1, 44]
-const query=[2, 5]
+const arr = [5, 7, 5, 2, 10, 12 ,11, 17, 14, 1, 44]
+const query=[1,3]
 
 const findLcm = (arr, query) => {
     let res = [];
@@ -13,9 +13,13 @@ const findLcm = (arr, query) => {
         res.push(arr[i]);
     } 
     res.sort((a, b) => a - b);
-    let lcm=res[res.length]
-   
+    let lcm = res[0];
+    for (let i = 0; i <res.length; i++){
+        if (res[i] % lcm !== 0) {
+            return 1;
+        }
 
-
+    }
+    return lcm;
 }
 console.log(findLcm(arr,query))
