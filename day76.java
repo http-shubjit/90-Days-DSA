0aeaa95298cc3986c2c2837f4bbcadf05dca2d11
Shubhajit <shubjitgithub@gmail.com>
2024-07-13 22:41:21 +0530

    DSA

---
+++ day76.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Day76 {
+    public static void main(String[] args) {
+        String str = "listen";
+        char arr[] = str.toCharArray();
+        Arrays.sort(arr);
+        System.out.println(arr);
+    
+
+
+   }
+    
+}
