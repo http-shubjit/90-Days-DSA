22bfb496be6716a5332a53e903fbc38115e122a9
Shubhajit <shubjitgithub@gmail.com>
2024-07-14 07:43:11 +0530

    DSA

---
+++ Day77.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day77 {
+ 
+    public static void plusMinus(List<Integer> arr) {
+        double plus = 0, minus = 0, zero = 0;
+        for (int i : arr) {
+            if (i > 0)
+                plus++;
+            else if (i < 0)
+                minus++;
+            else
+                zero++;
+        }
+        double pr = plus / (arr.size());
+        double mr = minus / (arr.size());
+        double zr = zero / (arr.size());
+
+        System.out.printf("%.6f%n", pr);
+        System.out.printf("%.6f%n", mr);
+        System.out.printf("%.6f%n", zr);
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<Integer>(Arrays.asList(-4, 3, -9, 0, 4, 1));
+        plusMinus(integers);
+    }
+
+}
+
--- day76.java
+++ day76.java
@@ -5,9 +5,22 @@ class Day76 {
         String str = "listen";
         char arr[] = str.toCharArray();
         Arrays.sort(arr);
-        System.out.println(arr);
-    
+        String s = new String(arr);
+        System.out.println(s);
 
+        String string = "silent";
+        char arr2[] = string.toCharArray();
+        Arrays.sort(arr2);
+        String s2 = new String(arr2);
+        System.out.println(s2);
+
+        if (s.equals(s2)) {
+            System.out.println("True");
+
+        }
+        else {
+            System.out.println("false");
+        }
 
    }
     
