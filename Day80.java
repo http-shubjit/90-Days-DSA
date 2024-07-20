7d39714b850027d51c8e028ceaad74545005ab29
Shubhajit <shubjitgithub@gmail.com>
2024-07-20 07:46:56 +0530

    DSA

---
+++ Day80.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+//**Hashmap**/
+
+
+// class HashmapCreation {
+//     public static void main(String[] args) {
+//         HashMap<String, Integer> hashMap = new HashMap<>();
+//         System.out.println("Before Insertion:" + hashMap);
+
+//         hashMap.put("Raka", 1);
+//         hashMap.put("Shubhajit", 2);
+//         hashMap.put("Chaturbhuja", 3);
+//         hashMap.put("Chanchala", 4);
+//         hashMap.put("Ranjit", 5);
+//         hashMap.put("Biswajit", 6);
+
+//         System.out.println("After Insertion:" + hashMap);
+
+// }
+    
+// }
+
+
+//**ArrayList**/
+
+/**
+ * Day80
+ */
+// class ArrayListCreation {
+
+//     public static void main(String[] args) {
+//         ArrayList<Integer> arrayList = new ArrayList<Integer>();
+//         for (int i = 0; i < 10; i++) {
+//             arrayList.add(i);
+//         }
+//         System.out.println(arrayList);
+//     }
+    
+// }
