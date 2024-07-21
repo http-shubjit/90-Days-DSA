0baa23be45c8c97fcba2e126b8ce77166c135546
Shubhajit <shubjitgithub@gmail.com>
2024-07-21 20:10:33 +0530

    DSA

--- Java/Collection/LiistClass.java
+++ Java/Collection/LiistClass.java
@@ -3,18 +3,23 @@ import java.lang.annotation.Documented;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 import java.util.Vector;
 
 abstract class LiistClass {
     public abstract void addElement(List<Integer> aList);
 
-    public abstract void print(List<Integer> aList,String string);
-    
+    public abstract void print(List<Integer> aList, String string);
+
+    public abstract void removeElement(List<Integer> aList);
 
 }
 
-class Main extends LiistClass {
+
+
+
+class ArrayListCreation extends LiistClass {
 
     @Override
     public void addElement(List<Integer> aList) {
@@ -23,32 +28,119 @@ class Main extends LiistClass {
         }
 
     }
+
     @Override
-    public void print(List<Integer> aList,String string) {
-        System.out.println("This is : "+string +"and Their Element are :");
-        for(int i:aList)
-        System.out.print(i+" ");
+    public void print(List<Integer> aList, String string) {
+        System.out.println("This is a " + string );
+            System.out.print(aList+"\n");
 
     }
 
-   
+    @Override
+    public void removeElement(List<Integer> aList) {
+        aList.remove(0);
+        aList.remove(1);
+        aList.remove(2);
+        aList.remove(3);
+
+    }
+
+}
+
+class LinkedListCreation extends LiistClass {
+
+    @Override
+    public void addElement(List<Integer> aList) {
+        for (int i = 0; i < 10; i++) {
+
+            aList.add(i);
+        }
+
+    }
+
+    @Override
+    public void print(List<Integer> aList, String string) {
+        System.out.println("This is a " + string);
+        System.out.print(aList + "\n");
+    }
+
+    @Override
+    public void removeElement(List<Integer> aList) {
+        aList.remove(0);
+        aList.remove(1);
+        aList.remove(2);
+        aList.remove(3);
+
+    }
+
+}
+
+class VectorCreation extends LiistClass {
+
+    @Override
+    public void addElement(List<Integer> aList) {
+        for (int i = 0; i < 10; i++) {
+
+            aList.add(i);
+        }
+
+    }
+
+    @Override
+    public void print(List<Integer> aList, String string) {
+        System.out.println("This is a " + string);
+        System.out.print(aList + "\n");
+    }
+
+    @Override
+    public void removeElement(List<Integer> aList) {
+        aList.remove(0);
+        aList.remove(1);
+    }
     
+}
+
+class Main  {
+
     
     public static void main(String[] args) {
-        Main main = new Main();
+        ArrayListCreation arrayListCreation = new ArrayListCreation();
+        LinkedListCreation linkedListCreation = new LinkedListCreation();
+        VectorCreation vectorCreation = new VectorCreation();
 
         List<Integer> list = new ArrayList<>();
         List<Integer> linkList = new LinkedList<>();
         Vector<Integer> vector = new Vector<>();
         Vector<Integer> stackVector = new Stack<>();
+        
 
-        //Arraylist
-        main.addElement(list);
-        main.print(list,"Arraylist");
+        // //Arraylist
+        // arrayListCreation.addElement(list);
+        // arrayListCreation.print(list, "Arraylist and Their Element are :");
+        // arrayListCreation.removeElement(list);
+        // arrayListCreation.print(list, "Arraylist after remove element");
+
+        //Linkedlist
+        // linkedListCreation.addElement(linkList);
+        // linkedListCreation.print(linkList, "Linkedlist and Their Element are :");
+        // linkedListCreation.removeElement(linkList);
+        // linkedListCreation.print(linkList, "LinkList after remove element");
+
+        //Vector
+
+        // vectorCreation.addElement(vector);
+        // vectorCreation.print(vector, "Vector and Their Element are :");
+        // vectorCreation.removeElement(vector);
+        // vectorCreation.print(vector, "Vector after remove element");
+
+        
+
+
 
 
 
     }
+   
 
 
 
